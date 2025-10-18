953831f3a8cbd1474d425dd533354ec0d3e73264
Gabriel Nardes <lightsirius3@gmail.com>
2025-10-18 13:49:30 -0300

    Esquema básico da sessão de conversas

--- frontend/src/pages/chat/components/conversas-componentes/conversa.jsx
+++ frontend/src/pages/chat/components/conversas-componentes/conversa.jsx
@@ -1,7 +1,18 @@
-export default function Conversa({nome}) {
-    return (
-        <div className="conversaDiv">
-            <p>{nome} <i class="fa-solid fa-circle ativo"></i></p>
-        </div>
-    )
+export default function Conversa({nome, ativo}) {
+    if (ativo) {
+        return (
+            <div className="conversaDiv">
+                <span>{nome}</span> 
+                <i class="fa-solid fa-circle ativo" id='ativo'></i>
+            </div>
+            )
+    } else {
+        return (
+            <div className="conversaDiv">
+                <span>{nome}</span>
+                <i class="fa-solid fa-circle desativado" id='ativo'></i>
+            </div>
+            )
+    }
+
 }
--- frontend/src/pages/chat/components/conversas.jsx
+++ frontend/src/pages/chat/components/conversas.jsx
@@ -2,7 +2,10 @@ import Conversa from './conversas-componentes/conversa'
 export default function Conversas() {
     return (
         <div className="conversas" id="conversas">
-            <Conversa nome='Pública'/>
+            <div className='fantasma'></div>
+            <Conversa nome='sedran' ativo={true}/>
+            <Conversa nome='Pública' ativo={true}/>
+            <Conversa nome='Networking' ativo={true}/>
         </div>
     )
 }
