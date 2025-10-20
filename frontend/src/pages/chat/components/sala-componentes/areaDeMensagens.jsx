aa1d73823a5faafabaaab0783f700da7a8c4d3c8
Gabriel Nardes <lightsirius3@gmail.com>
2025-10-20 14:03:16 -0300

    Estrutura e layout das mensagens

--- frontend/src/pages/chat/components/sala-componentes/areaDeMensagens.jsx
+++ frontend/src/pages/chat/components/sala-componentes/areaDeMensagens.jsx
@@ -1,7 +1,10 @@
+import Mensagem from "./mensagens/mensagem";
+
+
 export default function  areaDeMensagem() {
     return (
         <div className="areaDeMensagem">
-            
+            {/* <Mensagem username='Gabriel' mensagem='Oi'/> */}
         </div>
     )
 }
