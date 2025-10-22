1831aa9d5a95f14507cf24570e17a410a81f6e44
Gabriel Nardes <lightsirius3@gmail.com>
2025-10-22 15:54:15 -0300

    corrreções e ajustes
        -Não existe masi rota de verificar usuário existente, isso se faz ao criar o usuário
        -auth.js retorna next() agora, funcional
        -Correção do token indo com espaço em branco ao comparar no auth.js

--- backend/src/routes/iaRoute.js
+++ backend/src/routes/iaRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const iaController = require('../controllers/iaController.js');
+const auth = require('../middlewares/auth.js');
 
 router.post('/ia/resposta', iaController.getAIChatResponse);
 router.post('/ia/sentimento', iaController.analyzeSentiment);
